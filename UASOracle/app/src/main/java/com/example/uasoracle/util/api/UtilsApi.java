f97a31c454a41e44c22db5b7d5d137a8689d0d84
SuciSukmaMelati <52520057+SuciSukmaMelati@users.noreply.github.com>
2019-08-03 14:31:40 +0700

    Update UtilsApi.java

--- UASOracle/app/src/main/java/com/example/uasoracle/util/api/UtilsApi.java
+++ UASOracle/app/src/main/java/com/example/uasoracle/util/api/UtilsApi.java
@@ -1,8 +1,8 @@
 package com.example.uasoracle.util.api;
 
 /**
- * Created by Sandri Alfarisi.
- * github : https://github.com/sandrialfarisi
+ * Created by Suci Sukma Melati.
+ * github : https://github.com/SuciSukmaMelati
  */
 public class UtilsApi {
 
